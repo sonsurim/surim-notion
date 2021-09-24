51eed56abdd4361678c9fba054fc3b66b75b951a
sonsurim <surim014@naver.com>
2021-09-25 03:40:07 +0900

    fix: ul 클릭 이벤트 방어코드 구현

--- src/components/sidebar/SidebarBody.js
+++ src/components/sidebar/SidebarBody.js
@@ -68,7 +68,7 @@ export default function SidebarBody({ $target, initialState, onClick }) {
 			const { id } = $li.dataset;
 			const { act } = e.target.dataset;
 
-			if (tagName === 'LI' || className.includes('blank')) {
+			if (tagName === 'LI' || tagName === 'UL' || className.includes('blank') ) {
 				return;
 			}
 
